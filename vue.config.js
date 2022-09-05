4f03c5119680f3dcb04f0535482cc3cd2426f046
yc <1726278683@qq.com>
2022-09-05 16:26:33 +0800

    init project

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+module.exports={
+  configureWebpack:{
+    resolve:{
+      alias:{
+        "@":path.resolve(__dirname,'src'),
+        view:'@/view',
+        router:'@/router',
+        components:'@/assets'
+      }
+    }
+  },
+  devServer:{
+    proxy:{
+      "/api":{
+        target:'http://152.136.185.210:4000',
+        pathRewrite:{
+          "^/api":""
+        },
+        changeOrigin:true
+      }
+    }
+  },
+  publicPath:'./',
+  outputDir:'dist',
+  asserts:'static'
+}
